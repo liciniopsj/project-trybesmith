8dd4b1aa88f107276cec545df43db94f6ca2927a
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2023-04-24 19:27:18 -0300

    model interfaces

---
+++ src/interfaces/orders.interface.ts
@@ -0,0 +1,5 @@
+/* eslint-disable camelcase */
+export default interface Orders {
+  id: number,
+  user_id: number,
+}
---
+++ src/interfaces/products.interface.ts
@@ -0,0 +1,7 @@
+/* eslint-disable camelcase */
+export default interface Product {
+  id: string,
+  name: number,
+  amount: string,
+  order_id: number,
+}
